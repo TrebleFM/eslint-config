e3204dcddb55addd5130f1bf92da6724b266a463
Spencer Hakim <spencer@treble.fm>
2016-11-21 17:23:38 -0500

    Removed useless rule

--- browser.js
+++ browser.js
@@ -9,9 +9,6 @@ module.exports = {
     "plugins": [
         "html"
     ],
-    "rules": {
-        "comma-dangle": [2, "never"]
-    },
     "env": {
         "es6": false,
         "node": false,
