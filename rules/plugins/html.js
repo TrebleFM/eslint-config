860e2e10ba8d66ddd79a4c6005758c3ceeabc1e8
Spencer Hakim <spencer@treble.fm>
2017-09-05 15:25:41 -0400

    Add doc comments for HTML rules

--- rules/plugins/html.js
+++ rules/plugins/html.js
@@ -2,12 +2,15 @@
 module.exports = {
     "plugins": ["html"],
     "settings": {
-        "html": {
-            // "html-extensions": [],
-            // "xml-extensions": [],
-            "indent": "+4",
-            "report-bad-indent": 1
-            // "javascript-mime-types": []
-        }
+        // Consider files ending with these extensions as HTML
+        // "html/html-extensions": [],
+        // Consider files ending with these extensions as XML
+        // "html/xml-extensions": [],
+        // Ensure that every script tags follow an uniform indentation
+        "html/indent": "+4",
+        // By default, this plugin won't warn if it encounters a problematic indentation, use in conjunction with the indent rule
+        "html/report-bad-indent": 1
+        // Customize the types that should be considered as JavaScript by providing one or multiple MIME types. If a MIME type starts with a /, it will be considered as RegExp
+        // "html/javascript-mime-types": []
     }
 };
