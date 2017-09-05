5a3cff324955070551a8b01af5d30d2ef03f3c3b
Spencer Hakim <spencer@treble.fm>
2017-09-05 14:40:22 -0400

    Move extends to top of files

--- rules/eslint/browser-babel.js
+++ rules/eslint/browser-babel.js
@@ -1,11 +1,11 @@
 // Modified version of ./node.js rules
 module.exports = {
+    "extends": "./browser.js",
     "env": {
         "commonjs": true
     },
     "parserOptions": {
         "ecmaVersion": 8,
         "sourceType": "module"
-    },
-    "extends": "./browser.js"
+    }
 };
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 module.exports = {
+    "extends": "./default.js",
     "env": {
         "mocha": true
-    },
-    "extends": "./default.js"
+    }
 };
